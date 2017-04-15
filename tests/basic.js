831e59fdbddae46dde1698e469b1f0bdf461a465
Philippe Charrière <ph.charriere@gmail.com>
2017-04-15 22:38:19 +0200

    Update basic.js

--- tests/basic.js
+++ tests/basic.js
@@ -4,6 +4,6 @@ const assert = require('chai').assert;
 describe("geek value equals 42", () => {
   let geek = 42;
   it("should return 42", () => {
-    assert.equal(geek, 423)
+    assert.equal(geek, 42)
   });
 });
